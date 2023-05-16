01532bf204ef94c811c470b463c8464bc896d0fb
IyadA <iyada@radware.com>
2023-05-16 07:26:08 +0300

    create empty project

---
+++ src/main/java/com/img/SmartFoodInventory/SmartFoodInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.img.SmartFoodInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFoodInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFoodInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/img/SmartFoodInventory/SmartFoodInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.img.SmartFoodInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFoodInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
