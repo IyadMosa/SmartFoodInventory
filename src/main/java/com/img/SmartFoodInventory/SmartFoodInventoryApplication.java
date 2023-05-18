ed6f0a0ae80799dcdd3dadd879c5bffbbee88cdc
IyadA <iyada@radware.com>
2023-05-18 10:46:37 +0300

    load items and users in startup

--- src/main/java/com/img/SmartFoodInventory/SmartFoodInventoryApplication.java
+++ src/main/java/com/img/SmartFoodInventory/SmartFoodInventoryApplication.java
@@ -1,13 +1,32 @@
 package com.img.SmartFoodInventory;
 
+import com.img.SmartFoodInventory.dto.UserDTO;
+import com.img.SmartFoodInventory.model.MyUser;
+import com.img.SmartFoodInventory.service.ItemService;
+import com.img.SmartFoodInventory.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class SmartFoodInventoryApplication {
 
+	@Autowired
+	private ItemService itemService;
+
+	@Autowired
+	private UserService userService;
 	public static void main(String[] args) {
 		SpringApplication.run(SmartFoodInventoryApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() throws IOException {
+		List<UserDTO> users =  userService.loadUsers();
+		itemService.loadItems(users);
+	}
 }
